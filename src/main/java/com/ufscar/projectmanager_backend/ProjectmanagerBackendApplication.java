a6e52fb9ae4d4b2a3be2f4fe1a9876d67f7ea04a
mauricio-gallera <galmeidamauricio12@gmail.com>
2025-05-21 20:55:13 -0300

    first commit

---
+++ src/main/java/com/ufscar/projectmanager_backend/ProjectmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufscar.projectmanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmanagerBackendApplication.class, args);
+	}
+
+}
